dd75b54a589646982d209fa9ba660ebbe41f834d
DANNJUGUNA <ndan30071@gmail.com>
2023-02-07 08:50:49 +0300

    lab completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+   print "Hello, #{name}!"
+end
+
+def greet_with_default (name="programmer")
+    print "Hello, #{name}!"
+end
+
+def add(a,b)
+    return a+ b
+end
+def halve(num)
+    if num.class != Integer
+      return nil
+    end
+  
+    num/2
+end
+greet_programmer
+greet("Jimmy")
+greet_with_default
+greet_with_default("Dan")
+add(2,5)
+halve(4)
+halve("sam")
